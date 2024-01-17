4afa25118f958adcbf7e616f5b76c66fe299871f
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-01-18 03:35:43 +0530

    using react-h-f & some validation & extra features

--- apps/web/app/(lobby)/dashboard/page.tsx
+++ apps/web/app/(lobby)/dashboard/page.tsx
@@ -1,62 +1,166 @@
 "use client";
 import { trpc } from "@notebook/trpc/trpc/client";
-import { Button, Shell, Skeleton, useToast } from "@notebook/ui/components";
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+  Shell,
+  Skeleton,
+  useToast,
+} from "@notebook/ui/components";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const notionToNextraInputSchema = z.object({
+  pageId: z.string().min(32).max(36),
+  rootRoute: z.object({
+    name: z.string().refine(
+      (name) => {
+        // Check if name is empty
+        if (!name) return false;
+
+        // Check if name starts with a period or a hyphen
+        if (
+          name.startsWith(".") ||
+          name.startsWith("-") ||
+          name.startsWith("/")
+        )
+          return false;
+
+        // Check if name contains invalid characters
+        const invalidCharacters = /[\:*?"<>|]/;
+        if (invalidCharacters.test(name)) return false;
+
+        // Check if name exceeds maximum length
+        if (name.length > 255) return false;
+
+        return true;
+      },
+      {
+        message: "Invalid directory name",
+      }
+    ),
+    title: z.string(),
+  }),
+  token: z.string().length(50),
+});
 
 export default function IndexPage() {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV || process.env.VERCEL_ENV) {
     return <div>Dashboard is not accessible in production yet.</div>;
   }
-  if (!process.env.NEXT_PUBLIC_NOTION_PAGE_ID) {
-    throw new Error(
-      "Please correctly set your NEXT_PUBLIC_NOTION_PAGE_ID in your .env"
-    );
-  }
-  const { toast } = useToast();
-  const pageId = process.env.NEXT_PUBLIC_NOTION_PAGE_ID;
-  const [isLoading, setIsLoading] = useState(false);
-  const queryInfo = trpc.useQueries((t) => [
-    t.notion.notionToNextra(
-      { pageId },
-      { enabled: false, trpc: { abortOnUnmount: true } }
-    ),
-  ]);
 
-  const refetchData = async () => {
-    setIsLoading(true);
-    await queryInfo[0].refetch();
-    setIsLoading(false);
-  };
-  const data = queryInfo[0]?.data?.msg;
+  const { toast } = useToast();
+  const N2N = trpc.notion.notionToNextra.useMutation();
+
   useEffect(() => {
-    if (data) {
+    if (N2N.data?.msg) {
       toast({
         variant: "success",
         title: "Success",
-        description: data,
+        description: N2N.data?.msg,
       });
     }
-  }, [data]);
+  }, [N2N.data?.msg]);
+
+  const form = useForm<z.infer<typeof notionToNextraInputSchema>>({
+    resolver: zodResolver(notionToNextraInputSchema),
+    defaultValues: {
+      rootRoute: { name: "docs", title: "Documentation" },
+    },
+  });
   return (
-    <Shell as={"div"} className="flex flex-col">
-      <div className="flex gap-4 items-center">
-        <Button
-          size="sm"
-          onClick={() => {
-            refetchData();
-          }}
-          disabled={isLoading}
+    <Shell as={"div"} className="flex flex-col items-center">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((data) => {
+            N2N.mutate({
+              pageId: data.pageId,
+              rootRoute: data.rootRoute,
+              token: data.token,
+            });
+          })}
+          className="w-1/3 sm:w-1/4 sm:space-y-3"
         >
-          {isLoading ? <Skeleton>Generating...</Skeleton> : "Generate Docs"}
-        </Button>
-        {!isLoading && data && <span>{data}</span>}
-      </div>
-      <Link href={"/docs"}>
-        <Button variant="outline" size="sm">
-          Go to docs
-        </Button>
-      </Link>
+          <FormField
+            control={form.control}
+            name="token"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Notion Integration Token</FormLabel>
+                <FormControl>
+                  <Input placeholder="secret_gibberish" required {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="pageId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Root Notion Page Id</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="3322a16bb55e40b7b108c9f70730bcc3"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="rootRoute.name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Root Route Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="mydocs" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="rootRoute.title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Route Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Documentation" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-between pt-3 items-center">
+            <Button type="submit" disabled={N2N.isLoading}>
+              {N2N.isLoading ? (
+                <Skeleton>Generating Docs...</Skeleton>
+              ) : (
+                "Generate Docs"
+              )}
+            </Button>
+            <Button variant="outline" size="sm">
+              <Link href={"/docs"}>Go to docs</Link>
+            </Button>
+          </div>
+        </form>
+      </Form>
+      {!N2N.isLoading && N2N.data?.msg && <span>{N2N.data.msg}</span>}
     </Shell>
   );
 }
